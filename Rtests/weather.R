7f5e7876a8b9abea51998939c9b0c37143f8296c
Duncan Temple Lang <duncan@r-project.org>
2012-05-09 14:56:28 -0700

    parse the result

--- Rtests/weather.R
+++ Rtests/weather.R
@@ -1,5 +1,13 @@
 library(SSOAP)
 w = processWSDL("http://www.weather.gov/forecasts/xml/DWMLgen/wsdl/ndfdXML.wsdl")
 iface = genSOAPClientInterface(, w)
-iface@functions$LatLonListCityNames(2)
+txt = iface@functions$LatLonListCityNames(2)
+
+doc = xmlParse(txt)
+tmp = xmlSApply(xmlRoot(doc), xmlValue)
+els = strsplit(tmp, "[,|]")
+
+
+
+
 
